d11eefc7b11313f70240b2890c58ce7c01df061b
Peter Harbst <pharbst@2-F-7.42heilbronn.de>
2023-12-01 18:04:40 +0100

    Wip:	finally something that works

--- srcs/requirements/mariadb/mariadb_init.sh
+++ srcs/requirements/mariadb/mariadb_init.sh
@@ -2,3 +2,23 @@
 
 echo Hallo from the script
 
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	echo installing sql database
+	mysql_install_db
+fi
+service mariadb start
+sleep 5
+result=$(mariadb -sN -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$WP_USER');")
+
+if [ "$result" = "0" ]; then
+    mariadb -e "CREATE USER '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS';"
+fi
+
+if [ $(mariadb -e "SHOW DATABASES;" | grep '$WP_DB' -q) ]; then
+    echo "Database: '$WP_DB' already exists"
+else
+    echo "Database '$WP_DB' does not exist"
+	mariadb -e "CREATE DATABASE IF NOT EXISTS $WP_DB; GRANT ALL ON $WP_DB.* TO '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS'; FLUSH PRIVILEGES;"
+fi
+
+service mariadb stop
