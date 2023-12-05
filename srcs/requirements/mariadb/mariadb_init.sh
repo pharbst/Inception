4e08e5209d0cee60ddbc45793e97d619f0b378b0
Peter Harbst <pharbst@2-F-7.42heilbronn.de>
2023-12-05 17:45:57 +0100

    Fix:	first working state of mariadb

--- srcs/requirements/mariadb/mariadb_init.sh
+++ srcs/requirements/mariadb/mariadb_init.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-echo Hallo from the script
-
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo installing sql database
 	mysql_install_db
 fi
 service mariadb start
-sleep 5
+# sleep 5
 result=$(mariadb -sN -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$WP_USER');")
 
 if [ "$result" = "0" ]; then
     mariadb -e "CREATE USER '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS';"
 fi
-
-if [ $(mariadb -e "SHOW DATABASES;" | grep '$WP_DB' -q) ]; then
-    echo "Database: '$WP_DB' already exists"
-else
-    echo "Database '$WP_DB' does not exist"
-	mariadb -e "CREATE DATABASE IF NOT EXISTS $WP_DB; GRANT ALL ON $WP_DB.* TO '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS'; FLUSH PRIVILEGES;"
+echo creating database
+if [ -d /var/lib/mysql/$WP_DB ]; then
+	echo database already exists
 fi
+mariadb -e "CREATE DATABASE IF NOT EXISTS $WP_DB; GRANT ALL ON $WP_DB.* TO '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS'; FLUSH PRIVILEGES;"
+sleep 2
 
 service mariadb stop
+
+exec mysqld -uroot
